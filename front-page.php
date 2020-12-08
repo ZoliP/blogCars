f6c51396f6e96921131ab624fd8f9b588b500cc9
Pallo Zoltan <pallozoli@yaho.com>
2020-12-08 11:55:40 +0200

    header and footer (partial)

---
+++ front-page.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <title>Blog Cars Template</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Blog Cars Template">     
+    <!-- FontAwesome CSS-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+	<!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">	<!-- Theme CSS -->
+	<!-- Theme CSS-->	
+	<link rel="stylesheet" href="./wp-content/themes/blogCars/style.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<!-- Lato Regular Font -->
+	<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+	<!-- PT Sans Regular Font -->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+</head> 
+
+<body >
+    <header class="header">
+		<div class="container">
+			<div class="row">	 	
+				<div class="logo col-sm-4">
+				<a href="#home"><img src="./wp-content/themes/blogCars/assets/images/logo.png" alt="logo"></a>
+				</div>
+  		
+		  		<div class="menu col-sm-4">
+					<a href="#home">Home</a>
+					<a href="#recipes">Recipes</a>
+		  			<a href="#article">Article</a>
+		  			<a href="#contact">Contact</a>
+		  			<a href="#purchase">Purchase</a>
+  				</div> 
+  				
+  				<div class="search col-sm-4">
+	    			<a href="#search"><i class="fas fa-search"></i></a>    		
+  				</div>
+			</div>
+		</div>	
+	</header>
+
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="logo col-sm-4">
+				<a href="#home"><img src="./wp-content/themes/blogCars/assets/images/logo.png" alt="logo"></a>
+				</div>	
+
+				<div class="recent col-sm-4">
+					<h3>Recent posts</h3>
+				</div>	
+				
+				<div class="newsletter col-sm-4">
+					<h3>Newsletter</h3>
+				</div>	
+			</div>			
+		</div>
+	</footer>
+	
+</body>
+</html>
